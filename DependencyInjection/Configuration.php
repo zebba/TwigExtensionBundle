4016ba4d953bb86533dbc9827effe671fb9e5dac
zebba <zebba@hotmail.de>
2014-08-14 15:41:20 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zebba\TwigExtensionBundle\DependencyInjection;
+namespace Zebba\Bundle\TwigExtensionBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
