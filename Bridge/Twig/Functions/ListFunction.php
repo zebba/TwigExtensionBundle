d384baee548383060da9554167e691cde365465e
Sebastian <sebastiankuhlmann@web.de>
2014-08-22 03:56:06 +0200

    Turned list filters into functions

--- Bridge/Twig/Functions/ListFunction.php
+++ Bridge/Twig/Functions/ListFunction.php
@@ -1,66 +1,66 @@
 <?php
 
-namespace Zebba\Bundle\TwigExtensionBundle\Bridge\Twig\Filter;
+namespace Zebba\Bundle\TwigExtensionBundle\Bridge\Twig\Functions;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class ListFilter extends \Twig_Extension
+class ListFunction extends \Twig_Extension
 {
 	/** @var ContainerInterface */
 	private $container;
-	
+
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param ContainerInterface $container
 	 */
 	public function __construct(ContainerInterface $container)
 	{
 		$this->container = $container;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
-	 * @see Twig_Extension::getFilters()
+	 * @see Twig_Extension::getFunctions()
 	 */
-	public function getFilters()
+	public function getFunctions()
 	{
 		return array(
-			new \Twig_SimpleFilter('ulist', array($this, 'ulistFilter'), array('is_safe' => array('html'))),
-			new \Twig_SimpleFilter('olist', array($this, 'olistFilter'), array('is_safe' => array('html'))),
+			new \Twig_SimpleFunction('ulist', array($this, 'ulistFunction'), array('is_safe' => array('html'))),
+			new \Twig_SimpleFunction('olist', array($this, 'olistFunction'), array('is_safe' => array('html'))),
 		);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param \IteratorAggregate $list
 	 * @param array $attributes
 	 */
-	public function ulistFilter(\IteratorAggregate $list, array $attributes = array())
+	public function ulistFunction(\IteratorAggregate $list, array $attributes = array())
 	{
 		return $this->container->get('templating')->render('ZebbaTwigExtensionBundle:List:ulist.html.twig', array(
 			'list' => $list
 		));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param \IteratorAggregate $list
 	 * @param array $attributes
 	 */
-	public function olistFilter(\IteratorAggregate $list, array $attributes = array())
+	public function olistFunction(\IteratorAggregate $list, array $attributes = array())
 	{
 		return $this->container->get('templating')->render('ZebbaTwigExtensionBundle:List:olist.html.twig', array(
 			'list' => $list
 		));
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see Twig_ExtensionInterface::getName()
 	 */
 	public function getName()
 	{
-		return 'zebba_list_filter';
+		return 'zebba_list_function';
 	}
 }
