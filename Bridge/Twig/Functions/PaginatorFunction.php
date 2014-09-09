ed0a62c357ae6ffec60d401d9894eb9c7c46e2ab
Sebastian <sebastiankuhlmann@web.de>
2014-09-09 14:11:08 +0200

    Added Paginator function

---
+++ Bridge/Twig/Functions/PaginatorFunction.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Zebba\Bundle\TwigExtensionBundle\Bridge\Twig\Functions;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class PaginatorFunction extends \Twig_Extension
+{
+	/** @var ContainerInterface */
+	private $container;
+
+	/**
+	 * Constructor
+	 *
+	 * @param ContainerInterface $container
+	 */
+	public function __construct(ContainerInterface $container)
+	{
+		$this->container = $container;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Twig_Extension::getFunctions()
+	 */
+	public function getFunctions()
+	{
+		return array(
+			new \Twig_SimpleFunction('paginator_pages', array($this, 'pagesFunction')),
+		);
+	}
+
+	/**
+	 *
+	 * @param Paginator $paginator
+	 * @param integer $limit
+	 * @param array $attributes
+	 */
+	public function pagesFunction(Paginator $paginator, $limit)
+	{
+		return ($paginator->count % limit) ? floor(paginator.count / limit) + 1 : floor(paginator.count / limit) + 1;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Twig_ExtensionInterface::getName()
+	 */
+	public function getName()
+	{
+		return 'zebba_paginator_function';
+	}
+}
